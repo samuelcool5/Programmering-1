d013dd1233dd72140f04b30f923393dc2cb29a03
samuelcool5 <samuel.evertsson03@gmail.com>
2022-03-18 15:37:12 +0100

    Detects damage loss
    Detects damage loss and sets that value as the new current health.
    
    Also filters out if it fails to make read value to an int.

--- Text detection.py
+++ Text detection.py
@@ -5,30 +5,34 @@ from PIL import ImageGrab
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 frame = None
+Currenthealth = 100
 
 def lost_health(img):
-    Currenthealth = 100
+    global Currenthealth
     txt = pytesseract.image_to_string(img)
     health = int(txt)
     if txt == " ":
         return False
     if health < Currenthealth:
         Currenthealth = health
+        print(Currenthealth)
         return True
+    Currenthealth = health
     return False
     
     
-    
-
 
 while True:
-    img = ImageGrab.grab(bbox=(0, 900, 1100, 1100)) #x, y, w, h
+    img = ImageGrab.grab(bbox=(100, 700, 700, 900)) #x, y, w, h
     img_np = np.array(img)
     frame = cv2.cvtColor(img_np, cv2.COLOR_BGR2GRAY)
     cv2.imshow("frame", frame)
     if cv2.waitKey(1) & 0Xff == ord('q'):
         cv2.destroyAllWindows()
         break
+    try:
+        lost_health(frame)
+    except ValueError:
+        pass
 
-txt = pytesseract.image_to_string(frame)
-print(txt)
+
